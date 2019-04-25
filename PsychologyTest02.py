b7fb1933e990e0a14e7b3266041313a2acb7d01b
yahara2896 <yhr.mcz2896@outlook.jp>
2019-04-25 17:44:56 +0900

    選択肢の切り替えを可能に

--- PsychologyTest02.py
+++ PsychologyTest02.py
@@ -5,6 +5,9 @@ import sys
 import os
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl
+import webbrowser
 
 class Window(QWidget):
     
@@ -13,12 +16,13 @@ class Window(QWidget):
         self.initUI()
 
     def initUI(self):
+
+
         self.resize(500,500)
         self.move(200,200)
         self.setWindowTitle('Psychology Test')
 
-        self.Q = QLabel('あなたは18さいの青少年です．高校で最後に何か盗もうという話になりました．いかのうち何を盗みますか．')
-
+        self.Q = QLabel('あなたは現在，18歳の青少年です．高校最後の思い出に友達と一緒に何かを盗もうという話になりました．あなたは何を盗みますか?')
         self.btn1 = QPushButton('プリンター')
         self.btn2 = QPushButton('電動アシスト自転車')
         self.btn3 = QPushButton('とんかつソース')
@@ -46,12 +50,15 @@ class Window(QWidget):
     def select1(self):
         self.Q.setText('それはどこで盗みますか') 
         self.btn1.setText('電気屋')
-        self.btn2.setText('プリンターやさん')
+        self.btn2.setText('プリンター屋さん')
         self.btn3.setText('パソコン屋さん')
         self.btn4.setText('N/D')
         self.btn5.setText('ひとつ前の質問に戻る')
 
         self.btn4.setDisabled(True)
+        self.btn1.clicked.connect(self.answer)
+        self.btn2.clicked.connect(self.answer)
+        self.btn3.clicked.connect(self.answer)
         self.btn5.clicked.connect(self.select0)
 
     def select2(self):
@@ -64,20 +71,12 @@ class Window(QWidget):
         self.btn5.setText('ひとつ前の質問に戻る')
         
         self.btn4.setDisabled(True)
+        self.btn1.clicked.connect(self.answer)
+        self.btn2.clicked.connect(self.answer)
+        self.btn3.clicked.connect(self.answer)
         self.btn5.clicked.connect(self.select0)
+
     def select3(self):
-        
-        self.Q.setText('それはどこで盗みますか') 
-        self.btn1.setText('ギリシャ，アテネ')
-        self.btn2.setText('模型屋')
-        self.btn3.setText('お土産物屋さん')
-        self.btn4.setText('N/D')
-        self.btn5.setText('ひとつ前の質問に戻る')
-
-        self.btn4.setDisabled(True)
-        self.btn5.clicked.connect(self.select0)
-
-    def select4(self):
         
         self.Q.setText('それはどこで盗みますか') 
         self.btn1.setText('スーパー')
@@ -88,20 +87,48 @@ class Window(QWidget):
         
         self.btn4.setDisabled(True)
 
+        self.btn1.clicked.connect(self.answer)
+        self.btn2.clicked.connect(self.answer)
+        self.btn3.clicked.connect(self.answer)
         self.btn5.clicked.connect(self.select0)
-    def select5(self):
+
+    def select4(self):
         
         self.Q.setText('それはどこで盗みますか') 
-        self.btn1.setText('電気屋')
-        self.btn2.setText('自転車屋')
-        self.btn3.setText('自転車置き場')
+        self.btn1.setText('ギリシャ，アテネ')
+        self.btn2.setText('模型屋')
+        self.btn3.setText('お土産物屋さん')
         self.btn4.setText('N/D')
-        self.btn5.setText('N/D')
+        self.btn5.setText('ひとつ前の質問に戻る')
 
         self.btn4.setDisabled(True)
 
+        self.btn1.clicked.connect(self.answer)
+        self.btn2.clicked.connect(self.answer)
+        self.btn3.clicked.connect(self.answer)
         self.btn5.clicked.connect(self.select0)
 
+
+
+
+
+
+
+    def select5(self):
+        
+        self.Q.setText('それはどこで盗みますか') 
+        self.btn1.setText('盗まないためにどこにもない')
+        self.btn2.setText('やっぱり盗む')
+        self.btn3.setText('N/D')
+        self.btn4.setText('N/D')
+        self.btn5.setText('前の質問へ戻る')
+
+        self.btn4.setDisabled(True)
+        self.btn3.setDisabled(True)
+
+        self.btn1.clicked.connect(self.answer)
+        self.btn2.clicked.connect(self.select0)
+        self.btn5.clicked.connect(self.select0)
 
     def select0(self):
         
@@ -113,13 +140,42 @@ class Window(QWidget):
         self.btn5.setText('何も盗まない')
 
         self.btn4.setDisabled(False)
+        self.btn3.setDisabled(False)
 
         self.btn1.clicked.connect(self.select1)
         self.btn2.clicked.connect(self.select2)
         self.btn3.clicked.connect(self.select3)
         self.btn4.clicked.connect(self.select4)
         self.btn5.clicked.connect(self.select5)
+        
+    def answer(self):
 
+        self.btn1.hide()
+        self.btn2.hide()
+        self.btn3.hide()
+       # self.btn3.setText("はじめから")
+        self.btn4.setDisabled(False)
+        self.btn4.setText('もっと心理テストをしたい場合はこちら')
+        self.btn5.setText('欲しいものを買う')
+        
+
+        self.Q.setText("1つ目の質問はあなたの器の大きさ，2つ目の質問はあなたが今必要としているものがある場所を表しています．")
+        
+       # self.btn3.clicked.connect(self.select0)
+        self.btn4.clicked.connect(self.url)
+        self.btn5.clicked.connect(self.url2)
+
+    def url(self):
+
+        initurl = 'https://matome.naver.jp/odai/2134933856191786901'
+        webbrowser.open(initurl)
+        QCoreApplication.instance().quit
+        
+    def url2(self):
+
+        initurl2 = 'https://www.amazon.co.jp'
+        webbrowser.open(initurl2)
+        QCoreApplication.instance().quit
 
 if __name__ == '__main__':
 
